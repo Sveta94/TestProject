d7210cff6b7005f91c141e870bd77d46a5ab5a11
Sveta <sveta.avetisyan94@gmail.com>
2019-07-03 10:43:05 +0300

    ContactHelper created

---
+++ src/test/java/addressbook_web_tests/appmanager/ContactHelper.java
@@ -0,0 +1,60 @@
+package addressbook_web_tests.appmanager;
+
+import addressbook_web_tests.model.ContactData;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class ContactHelper  {
+    private WebDriver wd;
+
+    public ContactHelper(WebDriver wd) {
+
+        this.wd = wd;
+    }
+
+    public void returnToContactPage() {
+        wd.findElement(By.linkText("home page")).click();
+    }
+
+    public void submitContactCreation() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    public void fillContactForm(ContactData contactData) {
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
+        wd.findElement(By.name("nickname")).click();
+        wd.findElement(By.name("nickname")).clear();
+        wd.findElement(By.name("nickname")).sendKeys(contactData.getNickname());
+        wd.findElement(By.name("bday")).click();
+        new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData.getBdayDay());
+        wd.findElement(By.xpath("//option[@value='7']")).click();
+        wd.findElement(By.name("bmonth")).click();
+        new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getBdayMonth());
+        wd.findElement(By.xpath("//option[@value='February']")).click();
+        wd.findElement(By.name("byear")).click();
+        wd.findElement(By.name("byear")).clear();
+        wd.findElement(By.name("byear")).sendKeys(contactData.getBdayYear());
+        wd.findElement(By.name("notes")).click();
+        // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | name=notes | ]]
+        wd.findElement(By.name("notes")).clear();
+        wd.findElement(By.name("notes")).sendKeys(contactData.getNotes());
+        wd.findElement(By.name("address2")).click();
+        wd.findElement(By.name("address2")).clear();
+        wd.findElement(By.name("address2")).sendKeys(contactData.getAddress2());
+        wd.findElement(By.name("phone2")).click();
+        wd.findElement(By.name("phone2")).clear();
+        wd.findElement(By.name("phone2")).sendKeys(contactData.getPhone2());
+    }
+
+
+
+
+
+
+}
--- src/test/java/addressbook_web_tests/tests/ContactCreationTest.java
+++ src/test/java/addressbook_web_tests/tests/ContactCreationTest.java
@@ -10,9 +10,9 @@ public class ContactCreationTest extends TestBase {
   public void testContactCreation() throws Exception {
 
     app.getNavigationhelper().gotoContactPage();
-    app.fillContactForm(new ContactData("Svetlana", "Avetisyan", "Sveta", "GGG", "Ulitsa Yunikh Lenintsev", "+7915000000000", "testemail@example.com", "7", "February", "1994", "test", "test", "test"));
-    app.submitContactCreation();
-    app.returnToContactPage();
+    app.getContactHelper().fillContactForm(new ContactData("Svetlana", "Avetisyan", "Sveta", "GGG", "Ulitsa Yunikh Lenintsev", "+7915000000000", "testemail@example.com", "7", "February", "1994", "test", "test", "test"));
+    app.getContactHelper().submitContactCreation();
+    app.getContactHelper().returnToContactPage();
     app.getSessionHelper().logout();
   }
 
